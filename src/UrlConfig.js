5ef2982c87a4f9ba6631dc59d7de51b678fec201
silverYfobs <sahil@silverwebbuzz.com>
2023-02-27 11:17:58 +0530

    bug fix

--- src/UrlConfig.js
+++ src/UrlConfig.js
@@ -1,4 +1,4 @@
-export const BASE_URL_FOR_ADMIN = "http://localhost:5000/";
+export const BASE_URL_FOR_ADMIN = "http://43.205.253.220:5000/";
 
 export const ADMIN_GET_ALL_MAIN_IPO = "GetMainLineIpo";
 export const ADMIN_CREATE_MAIN_IPO = "createMainlineIPO/";
